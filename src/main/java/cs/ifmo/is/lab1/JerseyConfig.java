27155dabf275bef27a58151cd0c0ce5592914da3
KseniyaNesterenko <115107951+KseniyaNesterenko@users.noreply.github.com>
2024-12-19 21:21:12 +0300

    немного импорта

--- src/main/java/cs/ifmo/is/lab1/JerseyConfig.java
+++ src/main/java/cs/ifmo/is/lab1/JerseyConfig.java
@@ -4,11 +4,13 @@ import cs.ifmo.is.lab1.service.BookCreatureService;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 @ApplicationScoped
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         packages("cs.ifmo.is.lab1.controller");
+        register(MultiPartFeature.class);
         register(new AbstractBinder() {
             @Override
             protected void configure() {
